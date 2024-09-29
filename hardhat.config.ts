a8c99e4d89bbf3a46fc56d1b1c4126ec8a6ff641
Deadshot Ryker <ryker@zerolend.xyz>
2024-09-29 15:14:36 +0400

    added deployment and fixed deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-dependency-compiler";
 import "hardhat-deploy";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.12",
+  networks: {
+    base: {
+      url: `https://mainnet.base.org/`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+  dependencyCompiler: {
+    paths: ["@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol"],
+  },
+  etherscan: {
+    apiKey: {
+      base: process.env.BASESCAN_KEY || "",
+    },
+  },
 };
 
 export default config;
