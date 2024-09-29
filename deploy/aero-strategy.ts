d3c5472e84a6b88b1a56da60b28ed105b96c1234
Deadshot Ryker <ryker@zerolend.xyz>
2024-09-29 14:58:25 +0400

    initial commit

---
+++ deploy/aero-strategy.ts
@@ -0,0 +1,17 @@
+import { DeployFunction } from "hardhat-deploy/types";
+// import { getWalletBalances } from "../helpers";
+
+/**
+ * The following script runs before the deployment starts
+ */
+
+const func: DeployFunction = async function () {
+  //   const balances = await getWalletBalances();
+  console.log("\nAccounts");
+  //   console.log("========");
+  //   console.table(balances);
+};
+
+func.tags = ["aero-strategy"];
+
+export default func;
