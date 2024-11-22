a4eaa538ed3e0fe052d27b37145cfbb616cc98a4
nikhilbajaj31 <nikhil@zerolend.xyz>
2024-11-22 14:40:44 +0530

    feat: add new deployments for berachain AToken

--- scripts/upgrade/upgrade_atoken_berachain.ts
+++ scripts/upgrade/upgrade_atoken_berachain.ts
@@ -6,7 +6,7 @@ import fs from "fs/promises";
 async function main() {
   if (hre.network.name === "berachain_bartio") {
     const AToken = await hre.ethers.getContractAt("AToken", "0xEc93009f7cDc8b15A48cAB34680a0b60e49D3FC5")
-    const rewardVault = "0x0c853787bd9a9AE2A2478BC7Cbf70F35Ca5280f6";
+    const rewardVault = "0x9f37B421A0294cA7A568D1cb1F6562D2443891dC";
 
     const asset = await AToken.UNDERLYING_ASSET_ADDRESS();
     const treasury = await AToken.RESERVE_TREASURY_ADDRESS();
@@ -17,7 +17,7 @@ async function main() {
     const PoolConfigurator: IPoolConfigurator = await hre.ethers.getContractAt("IPoolConfigurator", "0x8aaF2E3080b64129D09cf5847065A20FD14F5a7D");
     const [deployer] = await hre.ethers.getSigners();
 
-    const newATokenImpl = getAddressFromJson("berachain_bartio", "ATokenBerachain");
+    const newATokenImpl = "0x2C2E2fC8A721d59F35d284446cACBDb6e7Ad582C";
 
     await PoolConfigurator.connect(deployer).updateAToken({
         asset: asset,
